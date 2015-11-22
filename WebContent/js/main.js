4fabaa7b45604caf5e1421ac55c39f17b5032adc
codigoalvo <codigoalvo@gmail.com>
2015-11-22 08:25:56 -0200

    Angular ngRoute

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,1 +1,22 @@
-angular.module('alvoApp', []);
+angular.module('alvoApp', ['ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+
+		//$locationProvider.html5Mode(true);
+
+		$routeProvider.when('/categorias', {
+			templateUrl: 'partials/categorias.html',
+			controller: 'CategoriasController'
+		});
+		
+		$routeProvider.when('/login', {
+			templateUrl: 'partials/login.html',
+			controller: 'LoginController'
+		});
+		
+		$routeProvider.when('/home', {
+			templateUrl: 'partials/home.html'
+		});
+		
+		$routeProvider.otherwise({redirectTo: '/home'});
+
+	});
