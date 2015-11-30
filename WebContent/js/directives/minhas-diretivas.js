c2ce9584ff66ae5df8bfce1362b736ff7a46a28b
codigoalvo <codigoalvo@gmail.com>
2015-11-30 15:34:55 -0200

    Corrigido menu de login, com conteúdo dinâmico dependendo de estar ou não logado.

--- WebContent/js/directives/minhas-diretivas.js
+++ WebContent/js/directives/minhas-diretivas.js
@@ -1,5 +1,5 @@
 angular.module('minhasDiretivas', [])
-	.directive('header', function (servicosLogin) {
+	.directive('header', function (servicosLogin, $compile) {
 		
 		var templateDeslogado = "<li ng-show='!usuarioLogado'><a href='#login'><span class='glyphicon glyphicon-log-in'></span> login </a></li>";
 		var templateLogado =	"<li ng-show='usuarioLogado' class='dropdown'>" +
@@ -12,53 +12,55 @@ angular.module('minhasDiretivas', [])
 										"<li><a href='#logout' ng-click='efetuarLogout()'><span class='glyphicon glyphicon-log-out'></span> logout </a></li>" +
 									"</ul>" +
 								"</li>";
-
-		var templateHtml = "<nav class='navbar navbar-default navbar-fixed-top'>" +
+		var templateAbre =	"<nav class='navbar navbar-default navbar-fixed-top'>" +
 								"<div class='container'>" +
 									"<div class='navbar-header'>" +
 										"<a class='navbar-brand' href='#home'>" +
 											"<img src='images/codigoalvo.svg' alt='codigoalvo' height='23px'/>" +
 										"</a>" +
-									"</div>" +
-									"<div id='navbar' class='navbar-collapse collapse'>" +
-										"<ul class='nav navbar-nav'>" +
-											"<li><a href='#home'><span class='glyphicon glyphicon-home' /></a></li>" +
-											"<li><a href='ws/categorias/1'>Categoria (1) - REST</a></li>" +
-											"<li><a href='ws/categorias'>Categorias - REST</a></li>" +
-											"<li><a href='#categorias'>Categorias - Angular</a></li>" +
-										"</ul>" +
-										"<ul class='nav navbar-nav navbar-right'>"+ /* ng-bind-html='templateMenuLogin'>" + */
-											templateDeslogado +
-											templateLogado +
-										"</ul>" +
-									"</div>" +
-								"</div>" +
-							"</nav>";
+									"</div>";
+		var templateFecha =	"</ul>" +
+						"</div>" +
+					"</div>" +
+				"</nav>";
+		
+		var templateOpcoes =	"<div id='navbar' class='navbar-collapse collapse'>" +
+									"<ul class='nav navbar-nav'>" +
+										"<li><a href='#home'><span class='glyphicon glyphicon-home' /></a></li>" +
+										"<li><a href='ws/categorias/1'>Categoria (1) - REST</a></li>" +
+										"<li><a href='ws/categorias'>Categorias - REST</a></li>" +
+										"<li><a href='#categorias'>Categorias - Angular</a></li>" +
+									"</ul>" +
+									"<ul class='nav navbar-nav navbar-right'>";
+
+		var templateHtml = "";
 
 		var ddo = {};
 			ddo.restrict = 'A';
 
 			ddo.replace = true;
-			ddo.template = templateHtml;
+			ddo.template = "";//templateHtml;
 			ddo.scope = {
 				templateMenuLogin: '=templateMenuLogin',
+				usuarioLogado: '=usuarioLogado',
 			}
 
-			/*
 			ddo.compile = function(element, attr) {
-				// manipulating template?
-
-				var	e = angular.element(templateMenuLogin);
-				element.append(e);
-
-				// the following is your linking function
 				return function($scope, element, attr) {
-					// template contains binding expressions? Yes
-					$compile(e)($scope);
+					templateHtml = "";
+					templateHtml += templateAbre;
+					templateHtml += templateOpcoes;
+					if ($scope.usuarioLogado) {
+						templateHtml +=	templateLogado;
+					} else {
+						templateHtml += templateDeslogado;
+					}
+					templateHtml +=	templateFecha;
+					var	htmlElement = angular.element(templateHtml);
+					element.html(htmlElement);
+					$compile(htmlElement)($scope);
 				};
 			};
-			*/
-			
 
 			ddo.controller = ['$scope', '$window', function ($scope, $window) {
 				var token = $window.sessionStorage.token;
@@ -67,19 +69,21 @@ angular.module('minhasDiretivas', [])
 				console.log('header.decodedToken', decodedToken);
 				$scope.templateMenuLogin = "";
 				if (decodedToken  &&  decodedToken.usuario) {
+					isLogado = true;
 					$scope.usuarioLogado = angular.fromJson(decodedToken.usuario);
 					$scope.myUser = $scope.usuarioLogado.login;
 					console.log('header.usuarioLogado', $scope.usuarioLogado);
-					/*$scope.templateMenuLogin = templateLogado;*/
+					$scope.templateMenuLogin = templateLogado;
 				} else {
+					isLogado = false;
 					$scope.usuarioLogado = undefined;
 					$scope.myUser = '';
-					/*$scope.templateMenuLogin = templateDeslogado;*/
+					$scope.templateMenuLogin = templateDeslogado;
 				}
 				$scope.efetuarLogout = function() {
 					servicosLogin.efetuarLogout();
 				}
 			}];
 		return ddo;
-	}
-);
+	})
+;
