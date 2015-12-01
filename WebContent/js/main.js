22e113c4eddc177fe64660e2ac4a3ccc0c0768c6
codigoalvo <codigoalvo@gmail.com>
2015-12-01 10:51:08 -0200

    CRUD de usuário 100% funcional e script para focus.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,4 +1,4 @@
-angular.module('alvoApp', ['ngRoute', 'ngResource', 'alvoRotas', 'authInterceptor', 'headerDirective', 'loginService', 'usuarioService', 'categoriaService'])
+angular.module('alvoApp', ['ngRoute', 'ngResource', 'alvoRotas', 'authInterceptor', 'miscDirectives', 'headerDirective', 'loginService', 'usuarioService', 'categoriaService'])
 	.config(function($httpProvider, $locationProvider) {
 		//$locationProvider.html5Mode(true);
 		//$httpProvider.interceptors.push('AuthInterceptor');
