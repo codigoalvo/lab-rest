f8b92f808af1a497e8f95b80e80eec6a623b082f
codigoalvo <codigoalvo@gmail.com>
2015-12-03 17:29:36 -0200

    * Corrigido bug da responsividade do google charts

--- WebContent/js/directives/googlechart-directive.js
+++ WebContent/js/directives/googlechart-directive.js
@@ -3,7 +3,7 @@ angular.module("google-chart",[])
 		return{
 			restrict : "A",
 			link: function postlink(scope, elem, attr){
-				scope.desenha = function() {
+				elem.desenha = function() {
 					var dt = scope[attr.ngModel].dataTable;
 
 					var options = {};
@@ -12,17 +12,16 @@ angular.module("google-chart",[])
 					if (scope[attr.ngModel].isStacked)
 						options.isStacked = scope[attr.ngModel].isStacked;
 
-					elem.width($window.innerWidth*0.7);
+					//elem.width($window.innerWidth*0.7);
 					//elem.height($window.innerHeight*0.6);
 
 					var googleChart = new google.visualization[attr.googleChart](elem[0]);
 					googleChart.draw(dt,options);
-					console.log('resizing');	
+					console.log('resizing: '+attr.ngModel);	
 				}
-				scope.desenha();
+				elem.desenha();
 				angular.element($window).bind('resize', function() {
-					scope.desenha();
-					scope.$apply();
+					elem.desenha();
 				});
 			}
 		}
