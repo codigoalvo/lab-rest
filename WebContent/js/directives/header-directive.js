6fd91b77bc5c5f21819c813a183a38b136d1c971
codigoalvo <codigoalvo@gmail.com>
2015-12-04 08:26:06 -0200

    * Corrige botão de menu do botstrap para dispositivos de tela pequena

--- WebContent/js/directives/header-directive.js
+++ WebContent/js/directives/header-directive.js
@@ -16,6 +16,12 @@ angular.module('headerDirective', [])
 		var templateAbre =	"<nav class='navbar navbar-default navbar-fixed-top'>" +
 								"<div class='container'>" +
 									"<div class='navbar-header'>" +
+										"<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>" +
+											"<span class='sr-only'>Toggle navigation</span>" +
+											"<span class='icon-bar'></span>" +
+											"<span class='icon-bar'></span>" +
+											"<span class='icon-bar'></span>" +
+										"</button>"+
 										"<a class='navbar-brand' href='#home'>" +
 											"<img src='images/codigoalvo.svg' alt='codigoalvo' height='23px'/>" +
 										"</a>" +
