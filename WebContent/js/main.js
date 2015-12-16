1558d151fc5031788cfff9e374796032d574e731
codigoalvo <codigoalvo@gmail.com>
2015-12-15 22:55:07 -0200

    Altera aparência dos painéis dos CRUD's e altera a aparência e a forma de funcionamento do angular-xeditable na tela de categorias

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -8,6 +8,9 @@ angular.module('alvoApp',	['ngRoute', 'ngResource', 'ngAnimate', 'ngMaterial', '
 		//$httpProvider.interceptors.push('AuthInterceptor');
 		growlProvider.globalTimeToLive({success: 3000, error: 5000, warning: 4000, info: 3000});
 		growlProvider.globalPosition('bottom-right');
-	}).run(function(editableOptions) {
-		editableOptions.theme = 'bs3'; // xeditable - bootstrap3 theme. Can be also 'bs2', 'default'
+	}).run(function(editableOptions, editableThemes) {
+		//editableOptions.theme = 'bs3'; // xeditable - bootstrap3 theme. Can be also 'bs2', 'default'
+		editableOptions.theme = 'default'; // set `default` theme
+		editableThemes['default'].submitTpl = '<button md-button type="submit" class="md-raised md-button-icon"><md-icon>save</md-icon></button>';
+		editableThemes['default'].cancelTpl = '<button md-button type="button" class="md-raised md-button-icon" ng-click="$form.$cancel()"><md-icon>reply</md-icon></button>'
 	});
